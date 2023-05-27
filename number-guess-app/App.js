f09a09341f258ba1e5ea39fdc0569a79c5f9b572
tcocco20 <theococco@yahoo.com>
2023-05-27 09:42:27 -0700

    added background and fixed layout

--- number-guess-app/App.js
+++ number-guess-app/App.js
@@ -1,8 +1,27 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
+import background from "./assets//images/background.png";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#3b021f", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        style={styles.rootScreen}
+        source={background}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
--- number-guess-app/screens/StartGameScreen.js
+++ number-guess-app/screens/StartGameScreen.js
@@ -9,8 +9,14 @@ function StartGameScreen() {
         maxLength={2}
         keyboardType="number-pad"
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -28,6 +34,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+    alignItems: "center",
+    justifyContent: "center",
   },
   numberInput: {
     height: 50,
@@ -39,5 +47,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
