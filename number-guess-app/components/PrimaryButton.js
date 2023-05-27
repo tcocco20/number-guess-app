5bf5099145a2262793136fa9a41e27ce5aef30cf
tcocco20 <theococco@yahoo.com>
2023-05-26 17:01:53 -0700

    added feedback for button.

---
+++ number-guess-app/components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import { Pressable, Text, View, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  const pressHandler = () => {
+    console.log("PrimaryButton pressed");
+  };
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
